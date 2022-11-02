3991440f3421e039cd2cc386a9d679dce0772495
SANKET MALI <sanketsmali@outlook.com>
2022-11-02 16:44:39 +0530

    first commit

--- tests/todomvc.spec.js
+++ tests/todomvc.spec.js
@@ -1,13 +1,14 @@
 const percySnapshot = require('@percy/playwright');
+let scrollToBottom = require("scroll-to-bottomjs");
 const { test, expect } = require('@playwright/test');
 
 const PORT = process.env.PORT_NUMBER || 8000;
 const TEST_URL = `https://www.dolcegabbana.com/en/`;
-const TEST_URL2 = `http://dev-dg.thelevelgroup.com/en/`;
+const TEST_URL1 = `http://dev-dg.thelevelgroup.com/en/`;
 
 test.beforeAll(async () => {
   // Set timeout for this hook.
-  test.setTimeout(120000);
+  test.setTimeout(150000);
 });
 
 test.beforeEach(async ({ page }, testInfo) => {
@@ -27,55 +28,92 @@ test.describe('TodoMVC', function () {
 
   test('Lets you check off a todo', async function ({ page }) {
    
-   //await page.setViewport({ width: 1440, height: 739 })
 
-    //await navigationPromise
+   await percySnapshot(page, 'Home Page Cookie');
 
-  //await page.waitForSelector('.fancybox-close')
-   // await page.click('.fancybox-close')
+   await page.waitForSelector('.cookiebanner__buttons__accept')
+   await page.click('.cookiebanner__buttons__accept')
 
-
-    await page.waitForSelector('.cookiebanner__buttons__accept')
-    await page.click('.cookiebanner__buttons__accept')
-
-    
-
+   //await page.waitForSelector('.fancybox-close ')
+    //await page.click('.fancybox-close')
 
     await page.waitForSelector('.b-header-strip-on-banner_close')
     await page.click('.b-header-strip-on-banner_close')
 
     await page.waitForSelector('.js-language_selector-flyout-close')
     await page.click('.js-language_selector-flyout-close')
+
+    await percySnapshot(page, 'Home Page');
     
+    //await percySnapshot('header', {scope:'div.b-primary_logo-container_desktop'});
 
     await page.waitForSelector('#category-level_2-kids')
     await page.click('#category-level_2-kids')
 
+    await page.waitForSelector('#link-food-beverage')
+    await page.click('#link-food-beverage')
+
+    await page.evaluate(scrollToBottom);
+
+    await percySnapshot(page, 'food & berverage');
+
+
+    await page.goto(TEST_URL);
+
+
+    await page.waitForSelector('#link-beauty')
+    await page.click('#link-beauty')
+
+
+    await page.evaluate(scrollToBottom);
+
+    await percySnapshot(page, 'beauty');
+
+
+    await page.goto(TEST_URL);
+
+    await page.waitForSelector('#link-fashion')
+    await page.click('#link-fashion')
+    
+
+    await page.evaluate(scrollToBottom);
+
+    await percySnapshot(page,'fashion');
+
+    await page.goto(TEST_URL);
+
     await page.waitForSelector('#link-casa-home')
     await page.click('#link-casa-home')
 
-      // Clicking the link will indirectly cause a navigation.
-    await page.locator('//a[@aria-label="dg casa colazione"]').click();
+    await percySnapshot(page, 'Case Home');
+    
+   /* await page.locator('//a[@aria-label="dg casa colazione"]').click();
     
    await page.locator('//span[@id="category-level_2-casa-home-arredamento"]').click();
 
-    //await page.locator('(//img[@title="Porcelain Espresso Set"])[3]').click();
+   await percySnapshot(page, 'Category');
 
-    await page.locator('//span[@id="category-level_2-casa-home-living"]').click();
+   // await page.locator('//span[@id="category-level_2-casa-home-living"]').click();
 
-    await page.locator("//div[@aria-label='Visit Candles page']").click();
+   // await page.evaluate(scrollToBottom);
 
-    await page.locator("(//a[@title='Scented Candle - Sicilian Orange'])[1]").click();
+  //  await percySnapshot(page, 'home living');*/
 
-    await page.locator("//button[@title='Add to cart']").click();
-
-   // await page.locator("//a[@title='Proceed to checkout']").click();
 
 
 
     
 
     
+
+   
+
+   
+
+
+   
+
+
 
     
   });
